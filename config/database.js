a1965509da64773563d8dd40eb85d35737f94538
brupadiro <brunodiharce@gmail.com>
2023-03-15 01:12:05 -0300

    g

--- config/database.js
+++ config/database.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
+      host: 'db-postgresql-nyc1-64675-do-user-12753330-0.b.db.ondigitalocean.com',
+      port: '25060',
+      database: 'kennen',
+      user: 'doadmin',
+      password: 'AVNS_YFfHjZAV9pUg5xsgaTU',
+      ssl: {
+        ca: fs.readFileSync(`./config/ca-certificate.crt`).toString(),
+      },
+},
     useNullAsDefault: true,
   },
 });
